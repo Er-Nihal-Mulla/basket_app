cfcb880a06b04240cebbe739de6fc46a8630a9b6
Mr. Nihal Mulla <bablack667@gmail.com>
2021-07-21 19:03:57 +0530

    reactjs project

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Footer from './Footer';
+import GroceryListLive from './GroceryListLive';
+import BasketListLive from './BasketListLive';
+
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+
+				<header className="App-header">
+					<i className="fa fa-shopping-basket fa-6" aria-hidden="true"></i>
+					<h1 className="App-title">Hello, Basket!</h1>
+				</header>
+
+				<nav>
+					<form>
+						<input type="text" name="filterIt" defaultValue="Set Filter" onChange={this.addToBasket} />
+					</form>
+				</nav>
+
+				<main>
+					<GroceryListLive />
+					<BasketListLive />
+				</main>
+
+				<Footer />
+
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FilterLink from './FilterLink';
+
+const Footer = () => (
+	<footer>
+		<div>
+			<FilterLink filter="SHOW_ALL">Footer</FilterLink>
+
+		</div>
+	</footer>
+);
+
+export default Footer;
